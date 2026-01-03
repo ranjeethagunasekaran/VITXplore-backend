68e712c137033a94cb3cdd5abc88ee6afd6a218b
ranjeethagunasekaran <ranjeethagunasekaran@gmail.com>
2026-01-03 22:33:18 +0530

    mail recover register

--- controllers/authController.js
+++ controllers/authController.js
@@ -6,11 +6,13 @@ const generateOtp = require('../utils/generateOtp');
 
 // ✅ Email Transporter
 const transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS
-    }
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true, // true for 465
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS // MUST be App Password
+  }
 });
 
 // ✅ Register Controller
